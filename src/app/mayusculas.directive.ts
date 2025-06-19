22b860bde75bb4b802f2f1dbde804bae3598f25a
Elmisa <trejomisaelperez2304@gmail.com>
2025-06-19 10:08:37 -0600

    cambios

---
+++ src/app/mayusculas.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appMayusculas]'
+})
+export class MayusculasDirective {
+
+  constructor() { }
+
+}
---
+++ src/app/tarea/tarea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea',
+  imports: [],
+  templateUrl: './tarea.component.html',
+  styleUrl: './tarea.component.css'
+})
+export class TareaComponent {
+
+}
